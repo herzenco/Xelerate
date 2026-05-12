9fd8f6e80099ebc3eb10ef02fefbd7207ef4cb9d
Herzen Cortes <herzen@Herzens-MacBook-Air.local>
2026-05-12 11:56:35 -0400

    Update Xelerate favicon assets

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,10 @@ export const metadata: Metadata = {
     card: "summary_large_image",
   },
   icons: {
-    icon: "/favicon.ico",
+    icon: [
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon.png", sizes: "180x180", type: "image/png" },
+    ],
     apple: "/apple-touch-icon.png",
   },
   robots: {
